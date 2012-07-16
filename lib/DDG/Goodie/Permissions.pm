2d6c4ee32f561617ee86373fbd82e2be4adaaee8
Dylan Lloyd <dylan@dylansserver.com>
2012-07-15 22:09:34 -0400

    Added permissions test, and made it pass

--- lib/DDG/Goodie/Permissions.pm
+++ lib/DDG/Goodie/Permissions.pm
@@ -8,7 +8,7 @@ zci is_cached => 1;
 zci answer_type => "permissions";
 
 my %modes = (
-    7 => "full",
+    7 => "read, write and execute",
     6 => "read and write",
     5 => "read and execute",
     4 => "read only",
@@ -18,23 +18,33 @@ my %modes = (
     0 => "none",
 );
 
+my %special_modes = (
+    '4' => 'setuid',
+    '2' => 'setgid',
+    '1' => 'sticky',
+    '0' => 'none',
+);
+
 handle query => sub { 
     return unless /^(?:chmod|permissions?) (\d{3,4})$/;
-    my $answer = '';
+    my $text = '';
     for (my $i = 0; $i < length $1; $i++) {
         my $mode = substr ($1, $i, 1);
         if (length $1 == 4) {
-            $answer .= "Special mode: $modes{$mode}<br>" if $i == 0;
-            $answer .= "Others may: $modes{$mode}<br>" if $i == 1;
-            $answer .= "Group may: $modes{$mode}<br>" if $i == 2;
-            $answer .= "Owner may: $modes{$mode}<br>" if $i == 3;
+            $text .= "Special mode: $special_modes{$mode}\n" if $i == 0;
+            $text .= "Owner may $modes{$mode}\n" if $i == 1;
+            $text .= "Group may $modes{$mode}\n" if $i == 2;
+            $text .= "Others may $modes{$mode}\n" if $i == 3;
         } else {
-            $answer .= "Others may: $modes{$mode}<br>" if $i == 0;
-            $answer .= "Group may: $modes{$mode}<br>" if $i == 1;
-            $answer .= "Owner may: $modes{$mode}<br>" if $i == 2;
+            $text .= "Owner may $modes{$mode}\n" if $i == 0;
+            $text .= "Group may $modes{$mode}\n" if $i == 1;
+            $text .= "Others may $modes{$mode}\n" if $i == 2;
         }
     }
-    return $answer, html => $answer;
+    chomp $text;
+    my $html = $text;
+    $html =~ s/\n/<br>/g;
+    return $text, html => $html;
 };
 
 1;
--- t/Permissions.t
+++ t/Permissions.t
@@ -12,10 +12,15 @@ ddg_goodie_test(
 	[qw(
 		DDG::Goodie::Permissions
 	)],
-	'chmod 777' => test_zci('THIS',
-        html => "THAT"),
-	'chmod 0644' => test_zci('THIS',
-        html => "THAT"),
+	'chmod 777' => test_zci('Owner may read, write and execute
+Group may read, write and execute
+Others may read, write and execute',
+        html => 'Owner may read, write and execute<br>Group may read, write and execute<br>Others may read, write and execute'),
+	'chmod 0644' => test_zci('Special mode: none
+Owner may read and write
+Group may read only
+Others may read only',
+        html => 'Special mode: none<br>Owner may read and write<br>Group may read only<br>Others may read only'),
 );
 
 done_testing;
